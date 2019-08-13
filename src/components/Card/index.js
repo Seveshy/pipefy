f9030d548432447a4a22f2ad01b1515c7c96b399
daniel <dev@digitalrepublic.com.br>
2019-08-13 16:44:48 -0300

    finish

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,70 @@
-import React from "react";
+import React, { useRef, useContext } from "react";
+import { useDrag, useDrop } from "react-dnd";
+
+import BoardContext from "../Board/context";
 
 import { Container, Label } from "./styles";
+import { unescapeComponent } from "uri-js";
 
-export default function Card() {
+export default function Card({ data, index, listIndex }) {
+  const ref = useRef();
+  const { move } = useContext(BoardContext);
+
+  const [{ isDragging }, dragRef] = useDrag({
+    item: { type: "CARD", index, listIndex },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+
+  const [, dropRef] = useDrop({
+    accept: "CARD",
+    hover(item, monitor) {
+      const draggedListsIndex = item.listIndex;
+      const targetListIndex = listIndex;
+
+      const draggedIndex = item.index;
+      const targetIndex = index;
+
+      if (
+        draggedIndex === targetIndex &&
+        draggedListsIndex === targetListIndex
+      ) {
+        return;
+      }
+
+      const targetSize = ref.current.getBoundingClientRect();
+      const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+
+      const draggedOffset = monitor.getClientOffset();
+      const draggedTop = draggedOffset.y - targetSize.top;
+
+      if (draggedIndex < targetIndex && draggedTop < targetCenter) {
+        return;
+      }
+
+      if (draggedIndex > targetIndex && draggedTop > targetCenter) {
+        return;
+      }
+
+      move(draggedListsIndex, targetListIndex, draggedIndex, targetIndex);
+
+      item.index = targetIndex;
+      item.listIndex = targetListIndex;
+    }
+  });
+
+  dragRef(dropRef(ref));
+
   return (
-    <Container>
+    <Container ref={ref} isDragging={isDragging}>
       <header>
-        <Label color="#7159c1" />
+        {data.labels.map(label => (
+          <Label key={label} color={label} />
+        ))}
       </header>
-      <p>Fazer migração completa do servidor</p>
-      <img
-        src="https://avatars1.githubusercontent.com/u/33661573?v=4"
-        alt="Adorable Avatars"
-      />
+      <p> {data.content} </p>
+      {data.user && <img src={data.user} alt="" />}
     </Container>
   );
 }
